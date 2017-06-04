7fba1944627226e547746ecfab2c26ece82121e9
Asier Manzano Arévalo <asier.ma.95@gmail.com>
2017-06-04 16:42:17 +0200

    Limitando fichas en drag and drop

--- practica3/funciones.js
+++ practica3/funciones.js
@@ -3,27 +3,20 @@
 if(sessionStorage['jugando']==null){
 
     sessionStorage.setItem('jugando', '0'); //JUGANDO || 0: COLOCAR FICHAS || 1: TURNO DE ROJO || 2: TURNO DE VERDE
-
 }
-
-
 
 if(sessionStorage['goles1']==null || sessionStorage['goles2']==null){
 
     sessionStorage.setItem('goles1','0');
-
     sessionStorage.setItem('goles2','0');
-
 }
 
 
 
 var fichas1; //fichas restantes en el banquillo del equipo verde
-
 var fichas2; //fichas restantes en el banquillo del equipo rojo
 
 var contFichasR = 0;
-
 var contFichasV = 0;
 
 function comprobarAccesoJuego(){
@@ -35,9 +28,7 @@ function comprobarAccesoJuego(){
 function dibujarCampoFutbol(){
 
     let cv = document.getElementById('campo'),
-
         ctx = cv.getContext('2d'),
-
         dim = cv.width / 20;
 
     //console.log(dim);
@@ -75,192 +66,109 @@ function dibujarCampoFutbol(){
     // lineas canvas terreno
 
     ctx.beginPath(); //para limpiar y que no se ponga todo del mismo color, tam, etc
-
     ctx.lineWidth = 1;
-
     ctx.strokeStyle = '#009378';
 
     for(let i = 0; i <= 20; i++){
-
-
-
         if(i >= 1 && i <= 19){
 
             //lineas verticales
-
             ctx.moveTo(dim * i, dim);
-
             ctx.lineTo(i * dim, cv.height);
 
-
-
             //lineas horizontales
-
             ctx.moveTo(dim, dim * i);
-
             ctx.lineTo(cv.width-dim, i * dim);
 
-
-
-            if(i >= 4 && i <= 7){
-
-                
+            if(i >= 4 && i <= 7){     
 
                 //porteria 1 lineas horizontales
-
                 ctx.moveTo(0, dim * i);
-
                 ctx.lineTo(dim, i * dim);
 
-
-
                 //porteria 2 lineas horizontales
-
                 ctx.moveTo(cv.width-dim, dim * i);
-
                 ctx.lineTo(cv.width, i * dim);
-
-
 
                 if(i == 4){
 
                     //porteria 1 lineas verticales 
-
                     ctx.moveTo(0, dim*4);
-
                     ctx.lineTo(0, dim*7);
 
-
-
                     //porteria 2 lineas verticales 
-
                     ctx.moveTo(cv.width, dim*4);
-
                     ctx.lineTo(cv.width, dim*7);    
 
                 }
-
             }
-
         }
-
     }
 
 
 
     //ctx.rect(1, 1, cv.width - 1, cv.height - 1);
-
     //ctx.rect(0, 0, cv.width - 1, cv.height - 1);
-
     ctx.stroke(); //pintar;
 
-
-
     // Círculo central 
-
     ctx.beginPath();
-
     ctx.lineWidth = 3;
-
     ctx.strokeStyle = '#1C44AC';
-
     ctx.arc(cv.width/2,(cv.height/2) + dim/2,25,0,2*Math.PI);
-
     ctx.stroke();
-
-
 
     // Semicirculo área 1
-
     ctx.beginPath();
-
     ctx.lineWidth = 3;
-
     ctx.strokeStyle = '#1C44AC';
-
     ctx.arc(dim*4,(cv.height/2) + dim/2,25,1.5*Math.PI,0.5*Math.PI);
-
     ctx.stroke();
-
-
 
     // Semicirculo área 2
-
     ctx.beginPath();
-
     ctx.lineWidth = 3;
-
     ctx.strokeStyle = '1C44AC';
-
     ctx.arc(dim*16,(cv.height/2) + dim/2,25,0.5*Math.PI,1.5*Math.PI);
-
     ctx.stroke();
 
-
-
     // Líneas de banda y de juego
-
     ctx.beginPath();
-
     ctx.lineWidth = 3;
-
     ctx.strokeStyle = '1C44AC';
 
     // También se pueden hacer estas lineas con un cuadrado,
-
     // pero lo he hecho así para probar nuevas formas.
-
     // izquierda
-
     ctx.moveTo(dim, dim);
-
     ctx.lineTo(dim, cv.height);
 
     // derecha
-
     ctx.moveTo(cv.width-dim, dim);
-
     ctx.lineTo(cv.width-dim, cv.height);
 
     // superior
-
     ctx.moveTo(dim, dim);
-
     ctx.lineTo(cv.width-dim, dim);
 
     // inferior
-
     ctx.moveTo(dim, cv.height);
-
     ctx.lineTo(cv.width-dim, cv.height);
 
     // área 1
-
     ctx.rect(dim, dim*3, dim*3, dim*5);
 
     // área 2
-
     ctx.rect(cv.width-dim*4, dim*3, dim*3, dim*5);
-
     ctx.stroke();
-
-
 
     // Línea central
-
     ctx.beginPath();
-
     ctx.lineWidth = 3;
-
     ctx.strokeStyle = '1C44AC';
-
     ctx.moveTo(cv.width/2, dim);
-
     ctx.lineTo(cv.width/2, cv.height);
-
     ctx.stroke();
-
-
 
 }
 
@@ -269,41 +177,26 @@ function dibujarCampoFutbol(){
 function redibujarCanvasTerreno(){
 
     let cv = document.getElementById('campo'),
-
         ctx = cv.getContext('2d'),
-
         img = new Image(),
-
         dim = cv.width / 20;
 
     //cv.width = cv.width;
 
-
-
     dibujarCampoFutbol();
 
-
-
     img.onload = function(){
-
         let dim = cv.width / 20,
-
             fila = Math.floor(y / dim),
-
             columna = Math.floor(x / dim);
 
         //ctx.drawImage(img,x,y);
 
         if(fila!=0 && columna!=0 && columna!=19){
-
             ctx.drawImage(img, columna*dim, fila*dim, dim, dim);
-
         }
-
     }
-
     img.src = document.getElementById(id).src;
-
 }
 
 
@@ -333,28 +226,19 @@ function prepararDragnDropFichas(){
     cv.ondragover = function(e) {
 
         e.preventDefault();
-
         e.stopPropagation();
 
         let x = e.offsetX,
-
             y = e.offsetY,
-
             ctx = cv.getContext('2d'),
-
             dim = cv.width / 20,
-
             fila = Math.floor(y / dim),
-
             columna = Math.floor(x / dim);
 
 
         console.log('dim:' + dim, 'fil:' + fila, 'col:' + columna);
 
-
-
         dibujarCampoFutbol();
-
 
         ctx.beginPath();
         ctx.strokeStyle = '#f00';
@@ -369,38 +253,31 @@ function prepararDragnDropFichas(){
     cv.ondrop = function(e){
 
         e.preventDefault();
-
         e.stopPropagation();
 
         let x = e.offsetX,
-
             y = e.offsetY,
-
             id = e.dataTransfer.getData('text/plain'),
-
             ctx = cv.getContext('2d'),
-
             img = new Image();
 
+        img.src = document.getElementById(id).src;
         //cv.querySelector('canvas').appendChild(document.getElementById(id));
 
-
-        img.onload = function(){
+        //img.onload = function(){
 
             let dim = cv.width / 20,
-
                 fila = Math.floor(y / dim),
-
                 columna = Math.floor(x / dim);
 
             //ctx.drawImage(img,x,y);
 
             if(fila!=0 && columna!=0 && columna!=19){
+                console.log('Fichas rojas:' + contFichasR);
                 muestraDado();
-
                 //ctx.drawImage(img, columna*dim, fila*dim, dim, dim);
-
-                if((columna>=1 && columna<=9) && contFichasR<5){
+                console.log(fila, columna);
+                if((columna>=1 && columna<=9 && id>=0 && id<=4) && contFichasR<5){
 
                     sessionStorage["ficha1y"] = fila;
                     sessionStorage["ficha1x"] = columna;
@@ -422,7 +299,7 @@ function prepararDragnDropFichas(){
                     botonPartida();
                 }
 
-                if((columna>=10 && columna<=18) && contFichasV<5){
+                if((columna>=10 && columna<=18 && id>=5 && id<=9) && contFichasV<5){
 
                     sessionStorage["ficha1y"] = fila;
                     sessionStorage["ficha1x"] = columna;
@@ -439,9 +316,7 @@ function prepararDragnDropFichas(){
                     ctx.strokeRect(columna * dim, fila * dim, dim, dim);
 
                     botonPartida();
-
                 }
-
             }
 
             //ctx.drawImage(img,0,0,cv.width,cv.height);
@@ -450,10 +325,12 @@ function prepararDragnDropFichas(){
 
             dibujarCampoFutbol();
 
-        }
+        //}
         console.log(id);
-        img.src = document.getElementById(id).src;
-        console.log(img.src.id);
+
+        //img.src = document.getElementById(id).src;
+
+        //console.log(img.src.id);
         //console.log(img.src);
 
         //console.log(id);
@@ -465,110 +342,65 @@ function prepararDragnDropFichas(){
 
 }
 
-
-
 var ficha = { //posicion de la ficha
     "fila": 0,
     "columna": 0
 };
 
-
-
 // 4 funciones para poder mover la ficha en el interior 
-
 // del canvas del terreno de juego
-
 /*function mouse_move(e){
 
     // return; //que no devuelva nada
-
     let cv = e.target,
-
         x = e.offsetX,
-
         y = e.offsetY,
-
         dim = cv.width / 20,
-
         fila = Math.floor(y / dim),
-
         columna = Math.floor(x / dim);
 
-
-
     //console.log(ficha.columna, ficha.fila);    
-
     //console.log(`Posicion: ${x} - ${y}`);
 
     if (cv.getAttribute('data-down')) { //ESTOY ARRASTRANDO LA FICHA
-
         console.log(`MOUSEMOVE=>Fila: ${fila} - columna: ${columna}`);
-
         if (ficha.columna != columna || ficha.fila != fila) {
-
             ficha.columna = columna;
-
             ficha.fila = fila;
-
             redibujarCanvasTerreno();
-
         }
-
     }
-
 }*/
-
-
-
-
 
 function mouse_click(e){
     //return;
 
     let cv = e.target,
-
         x = e.offsetX,
-
         y = e.offsetY,
-
         dim = cv.width / 20,
-
         fila = Math.floor(y / dim),
-
         columna = Math.floor(x / dim);
-
-
 
     // console.log(`Posicion: ${x} - ${y}`);
 
     // console.log(`Fila: ${fila} - columna: ${columna}`);
 
     if (x < 1 || x > cv.width - 1 || y < 1 || y > cv.height - 1){
-
-        return;   
-
+        return;
     }
 
     /*console.log(fila, columna);
-
     console.log(dim);*/
-
     //cv.width = cv.width; //limpiar canvas - solo deja poner una al limpiar el canvas
-
     //dibujarCampoFutbol(); // dibuja el campo de nuevo para eliminar el cuadro destacada de cada ficha
 
     let ctx = cv.getContext('2d'),
-
         //id = e.dataTransfer.getData('text/plain'),
-
         img1 = new Image();
-
         img2 = new Image();
 
-
-
     img1.src = 'fichaRoja.svg'; // document.getElementById(id).src
-
     img2.src = 'fichaVerde.svg';    
 
     //img.onload = function(){
